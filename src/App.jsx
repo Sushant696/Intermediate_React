fe857f8475fc7bdab8e89493339316039ff73b93
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-18 21:41:30 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Explore the hitesh choudary playlist plus practice some intermediate concepts too</h1>
+    </>
+  )
+}
+
+export default App
